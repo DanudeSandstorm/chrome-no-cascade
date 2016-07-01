ca4758bc1bde64e7284ed018916de4cd2cc96c4e
Daniel <dds2130@rit.edu>
2016-06-30 21:48:05 -0400

    fixed my problems by reducing code... now its 1 line of code

--- background.js
+++ background.js
@@ -1,21 +1,3 @@
-var w_top = 0, 
-    w_left = 0;
-
-chrome.windows.onFocusChanged.addListener(function(w){
-    if(w > 0) { //Checks window id to be valid
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            if (tabs.length > 0) {
-                chrome.tabs.sendMessage(tabs[0].id, {}, function(response) {
-                    if (response) {
-                        w_top = response.top;
-                        w_left = response.left;
-                    }
-                });
-            }
-        });
-    }
-});
-
-chrome.windows.onCreated.addListener(function(new_window) {
-    chrome.windows.update(new_window.id, {left: w_left, top: w_top});
+chrome.windows.onCreated.addListener(function(w) {
+    chrome.windows.update(w.id, {left: w.left - 10, top: w.top - 10});
 });
--- content.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        var left = window.screenLeft;
-        var top = window.screenTop;
-        sendResponse({top: top, left: left});
-    }
-);
