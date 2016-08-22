b1fea606b25f95be114168d9dead7b739958a2fb
Daniel <dds2130@rit.edu>
2016-08-22 17:25:20 -0400

    Fixed glitch with popout windows

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.windows.onCreated.addListener(function(w) {
-    if (w.state == "normal") {
+    if (w.state == "normal" && w.type == "normal") {
         chrome.windows.update(w.id, {left: w.left - 10, top: w.top - 10});
     }
 });
